1a6f81fc98f9d5700f7fa43991895ea983228ae5
William L. Anderson <band@acm.org>
2026-03-05 16:15:06 -0600

    baseline workable lib install script

---
+++ .workbench/shlib_install.sh
@@ -0,0 +1,25 @@
+# Shell library installation script
+# the main procedure is:
+# - download the release directory
+# - cd to release directory
+# - run `sh_install.sh`
+#
+shlib_install () {
+    : uses: sed
+    fullpath() { ( cd "$1" && pwd -P ) }
+    # get directory/path to hold the libraries
+    local lib_dir
+    read "lib_dir?Enter library install directory path: "
+    lib_dir=$(fullpath "${lib_dir/#\~/$HOME}")
+    local dest="$lib_dir/initlib"
+    # replace last line of `initlib` with "init_here $lib_dir"
+    { sed '$d' "$src"; print -r "init_here $lib_dir" } > "$dest"
+    # copy the libraries to $lib_path
+    local libs=(zshutilib backuplib versionlib releaselib)
+    for lib in $libs; do
+	cp -v $lib $lib_dir
+    done
+}
+shlib_install
+
+
